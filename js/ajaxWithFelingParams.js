576ec20f323e2d18c088e46091f3a73a7a7e756d
chenyan <chenyan@wecash.net>
2017-12-24 22:24:09 +0800

    。。

--- js/ajaxWithFelingParams.js
+++ js/ajaxWithFelingParams.js
@@ -3,23 +3,23 @@ var WithFParams = {
     apiHost: 'https://api.feling.io',
     postJson: function (path, data, resultHandler) {
         data.accessToken = localStorage.accessToken
-        $.ajax({
+        return $.ajax({
             type: 'POST',
             contentType: 'application/json;charset=utf-8',
             url: this.apiHost + path,
             data: JSON.stringify(data),
             success: resultHandler,
             dataType: 'json'
-          });
+        })
     },
     postForm: function (path, data, resultHandler) {
         data.accessToken = localStorage.accessToken
-        $.ajax({
+        return $.ajax({
             type: 'POST',
             url: this.apiHost + path,
             data: data,
             success: resultHandler,
             dataType: 'json'
-          });
+        })
     },
 }
